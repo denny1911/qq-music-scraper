4e55d52d76e914e4653665c7a5ed784ec2a9872e
denny1911 <enailwjjoop56497@gmail.com>
2026-08-11 17:08:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1071,7 +1071,7 @@ with main_tabs[3]:
                             .list(
                                 q=query_str,
                                 part="id",
-                                maxResults=10,
+                                maxResults=20,
                                 type="video",
                                 order="relevance",
                                 regionCode="TW"
